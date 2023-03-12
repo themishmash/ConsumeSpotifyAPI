eba999e35ddc8c0d0d7ef7f6055a8100d16f7cfc
themishmash <wong.mich.c@gmail.com>
2023-03-12 15:10:37 +1100

    first commit

---
+++ consumespotifyapi2/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using consumespotifyapi2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using consumespotifyapi2.Services;
+using Microsoft.Extensions.Configuration;
+
+namespace consumespotifyapi2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ISpotifyAccountService _spotifyAccountService;
+        private readonly IConfiguration _configuration;
+        private readonly ISpotifyService _spotifyService;
+
+        public HomeController(ISpotifyAccountService spotifyAccountService, IConfiguration configuration, ISpotifyService spotifyService)
+        {
+            _spotifyAccountService = spotifyAccountService;
+            _configuration = configuration;
+            _spotifyService = spotifyService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var newReleases = await GetReleases();
+            return View(newReleases);
+        }
+
+        private async Task<IEnumerable<Release>> GetReleases()
+        {
+            try
+            {
+                var token = await _spotifyAccountService.GetToken(_configuration["Spotify:ClientId"],
+                    _configuration["Spotify:ClientSecret"]);
+                var newReleases = await _spotifyService.GetNewReleases("AU", 20, token);
+
+                return newReleases;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return Enumerable.Empty<Release>();
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
