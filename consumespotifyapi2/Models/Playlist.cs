b3350121b41304855c09a648d1dd76e2c4f4deb6
themishmash <wong.mich.c@gmail.com>
2023-03-13 12:00:54 +1100

    Created Playlist model

---
+++ consumespotifyapi2/Models/Playlist.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace consumespotifyapi2.Models
+{
+    public class Playlist
+    {
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public string Link { get; set; }
+        public int TrackTotal { get; set; }
+    }
+}
